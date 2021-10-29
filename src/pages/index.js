b8267410d7efc9b829f1b4c12fb5ffdb84936851
Wellens Khayodele (s115491) <khayodele.wellens@student.ap.be>
2021-10-29 16:56:50 +0200

    eerste plugin gebruikt

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,18 @@
 // Stap 1: Importeer React
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 // Stap 2: definieer je component
 const IndexPage = () => {
   return (
     <main>
       
       <Layout pageTitle="Welcome to The Khayo Agency!">
-        <p>lorem ipsum dolor ipmat it dador itkan stukan</p>
+        <p>lorem ipsum dolor ismat</p>
+        <StaticImage
+        alt="grijze stad"
+        src="../images/gray_city.jpg"
+        />
       </Layout>
     </main>
   )
